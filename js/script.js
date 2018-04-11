56b2cc5226854e730fff4e5c8fda27a5e771d1ec
matsta25 <matim2112@gmail.com>
2018-04-11 02:02:45 +0200

    fix v1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var preloaderEl = document.querySelector('.preloaderSeciton');
+var mainSection = document.querySelector('.mainSection-showing');
+
 window.addEventListener('load', function () {
     preloaderEl.classList.add('preloader-hiding');
+    mainSection.classList.remove('mainSection-showing');
     preloaderEl.addEventListener('transitionend', function () {
         this.classList.add('preloader-hidden')
         this.classList.remove('preloader-hiding')
